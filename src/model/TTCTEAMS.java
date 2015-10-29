20ec2f1f6ab4b53ed05570dc36784864733d5ed1
satbeka <satbeka@gmail.com>
2015-10-29 19:10:20 +0600

    281015 db tabl

---
+++ src/model/TTCTEAMS.java
@@ -0,0 +1,34 @@
+package model;
+
+
+public class TTCTEAMS {
+    private String id;
+    private String vNAME;
+    private String vTEAMDESC;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getvNAME() {
+        return vNAME;
+    }
+
+    public void setvNAME(String vNAME) {
+        this.vNAME = vNAME;
+    }
+
+    public String getvTEAMDESC() {
+        return vTEAMDESC;
+    }
+
+    public void setvTEAMDESC(String vTEAMDESC) {
+        this.vTEAMDESC = vTEAMDESC;
+    }
+
+
+}
